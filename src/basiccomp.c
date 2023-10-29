e8aa8f31c080315812fd616097b18584c5e5c899
Bill Chatfield <bill_chatfield@yahoo.com>
2023-10-29 14:38:34 -0400

    Some progress

--- src/basiccomp.c
+++ src/basiccomp.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
 
-void main()
+#define MAX_PATH_LEN 64
+
+char basic_file_name[MAX_PATH_LEN+1];
+char assembly_file_name[MAX_PATH_LEN+1];
+
+FILE *basic_file;
+FILE *assembly_file;
+
+void main(int arg_count, char *args[])
 {
-	printf("Compiling...\n");
+	switch (arg_count) {
+		case 3:
+			basic_file_name = args[1];
+			assembly_file_name = args[2];
+			break;
+		case 2:
+			basic_file_name = args[1];
+			assembly_file_name = ask_user("ASSEMBLY OUTPUT FILE:");
+			break;
+		default:
+			basic_file_name = ask_user("BASIC INPUT FILE");
+			assembly_file_name = ask_user("ASSEMBLY OUTPUT FILE:");
+			break;
+	}
+
 }
 
